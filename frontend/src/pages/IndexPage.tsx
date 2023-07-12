048b39d9c16394348df4ef0bd4219341b1abe1a3
2603003199a <58811224+skybird23333@users.noreply.github.com>
2023-07-13 01:20:38 +0800

    Optimize search matching

--- frontend/src/pages/IndexPage.tsx
+++ frontend/src/pages/IndexPage.tsx
@@ -25,47 +25,48 @@ const IndexPage: Component = () => {
                         gifs().forEach((gif, i) => {
                             columns[i % columnCount].push(
                                 <a href={`/gifs/${gif.id}`}>
-                                    <GifPreviewSingle gif={gif} tryForceCache />
-                                    <For each={gif.tags}>
-                                        {(tag, i) => {
-                                            //SEARCH HIGHLIGHTING
-                                            //First see if the search is present in the tag
-                                            const search = tag.match(query())
-                                            if (!search) { //If not then return it normally
+                                    <div style="display: grid; grid-template-rows: auto auto">
+                                        <GifPreviewSingle gif={gif} tryForceCache />
+                                        <For each={gif.tags}>
+                                            {(tag, i) => {
+                                                //SEARCH HIGHLIGHTING
+                                                //First see if the search is present in the tag
+                                                const search = tag.match(query())
+                                                if (!search) { //If not then dont return it
+                                                    return (<></>
+                                                    )
+                                                }
+                                                //If so then split it into parts. (Could be multiple parts matching the query!)
+                                                const parts = tag.split(search[0])
+                                                    .reduce((list, elem, i) => {
+                                                        list.push(elem);
+                                                        if (i == list.length - 1)
+                                                            list.push(search[0]);
+                                                        return list;
+                                                    }, []);
+                                                console.log(parts)
                                                 return (
-                                                    <span class="tag">{tag}</span>
+                                                    <span class="tag">
+                                                        <For each={parts}>
+                                                            {(part, i) => (
+                                                                <span>
+                                                                    <Show when={part == search}>
+                                                                        <span style="background-color: rgba(255, 255, 0, 0.5)">
+                                                                            {part}
+                                                                        </span>
+                                                                    </Show>
+                                                                    <Show when={part != search}>
+                                                                        {part}
+                                                                    </Show>
+                                                                </span>
+                                                            )}
+                                                        </For>
+                                                    </span>
                                                 )
                                             }
-                                            //If so then split it into parts. (Could be multiple parts matching the query!)
-                                            const parts = tag.split(search[0])
-                                                .reduce((list, elem, i) => {
-                                                    list.push(elem);
-                                                    if (i == list.length - 1)
-                                                        list.push(search[0]);
-                                                    return list;
-                                                }, []);
-                                            console.log(parts)
-                                            return (
-                                                <span class="tag">
-                                                    <For each={parts}>
-                                                        {(part, i) => (
-                                                            <span>
-                                                                <Show when={part == search}>
-                                                                    <span style="background-color: rgba(255, 255, 0, 0.5)">
-                                                                        {part}
-                                                                    </span>
-                                                                </Show>
-                                                                <Show when={part != search}>
-                                                                    {part}
-                                                                </Show>
-                                                            </span>
-                                                        )}
-                                                    </For>
-                                                </span>
-                                            )
-                                        }
-                                        }
-                                    </For>
+                                            }
+                                        </For>
+                                    </div>
                                 </a>,
                             );
                             return;
