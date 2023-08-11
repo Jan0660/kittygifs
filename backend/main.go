64b9ecaa38e6d94cbb0b61f2030dcbcf339bcf48
Jan0660 <jan0660@tutanota.com>
2023-08-11 16:13:15 +0200

    feat(backend): wipe unverified accounts older than 24h

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"go.mongodb.org/mongo-driver/bson"
 	"kittygifs/routes"
 	. "kittygifs/util"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -28,6 +31,44 @@ func main() {
 	}
 	LoadEmailTemplates()
 	InitializeMongoDB(&config)
+	// wiping old unverified users
+	if config.Smtp != nil {
+		timer := time.NewTimer(24 * time.Hour)
+		go func() {
+			for {
+				<-timer.C
+				timer.Reset(24 * time.Hour)
+				log.Println("Wiping unverified users")
+				ctx := context.Background()
+				cur, err := UsersCol.Find(ctx, bson.M{"verification": bson.M{"$exists": true}})
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+				for cur.Next(ctx) {
+					var user User
+					err := cur.Decode(&user)
+					if err != nil {
+						log.Println(err)
+						continue
+					}
+					timestamp, err := GetVerificationTokenTimestamp(*user.Verification)
+					if err != nil {
+						log.Println(err)
+						continue
+					}
+					if timestamp.Add(24 * time.Hour).Before(time.Now()) {
+						_, err = UsersCol.DeleteOne(ctx, bson.M{"_id": user.Username})
+						if err != nil {
+							log.Println(err)
+							continue
+						}
+						log.Printf("Deleted user %s\n", user.Username)
+					}
+				}
+			}
+		}()
+	}
 	err := routes.RunGin(&config)
 	if err != nil {
 		log.Fatal(err)
